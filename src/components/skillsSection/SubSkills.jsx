b4f7911d2aadbe268278550ad4effa93946eb350
Daebie <chisomnduwuba3@gmail.com>
2025-04-16 18:29:18 +0100

    latest

--- src/components/skillsSection/SubSkills.jsx
+++ src/components/skillsSection/SubSkills.jsx
@@ -3,7 +3,7 @@ const SubSkills = () => {
     <div className="border-y-2 border-lightGrey relative ">
       <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full"></div>
       <img
-        src="../../public/images/subSkills.jpg"
+        src="https://res.cloudinary.com/dajtou6ym/image/upload/v1744824405/subSkills_xzvckb.jpg"
         alt="subSkills image"
         className=""
       />
